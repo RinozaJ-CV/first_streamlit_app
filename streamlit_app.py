bce96cc091e73945fa959e0326a5f28ab65aec5a
RinozaJ-CV <119654667+RinozaJ-CV@users.noreply.github.com>
2023-09-29 15:16:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import snowflake.connector
+import pandas
+from urllib.error import ULRError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -12,7 +14,7 @@ streamlit.text("🥑🍞 Avacado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
